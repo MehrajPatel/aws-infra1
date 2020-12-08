6669307eb12bc305e6f9648e8536e3f8202c2062
MehrajDevOps <mehrajpatel13@gmail.com>
2020-12-08 09:42:10 +0530

    changed ami for us-west-1

--- variables.tf
+++ variables.tf
@@ -2,11 +2,25 @@ variable "region" {
   type = string
 }
 
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "Learn"
+}
+
 variable "amis" {
   type = map
   default = {
     "us-east-1" = "ami-04d29b6f966df1537"
-    "us-west-1" = "ami-00831fc7c1e3ddc60"
+    "us-west-1" = "ami-0a741b782c2c8632d"
   }
 }
 
@@ -15,9 +29,10 @@ variable "aws_access_key_id" {
 }
 
 variable "aws_secret_access_key" {
-  type = string
+  type      = string
 }
 
 output "ami" {
   value = aws_instance.firstinstance.ami
 }
+
